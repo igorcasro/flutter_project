c1390b4331a586df903b900a89634d5d54c99d51
Igor Castro <igorpfcastro@gmail.com>
2022-10-04 12:04:33 -0300

    Consertando warnings (#7)
    * Consertando warnings

--- app_receitas/lib/main.dart
+++ app_receitas/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:app_receitas/constants.dart';
 import 'package:app_receitas/pages/login_page.dart';
 import 'package:flutter/material.dart';
 
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -20,10 +19,12 @@ class MyApp extends StatelessWidget {
       home: const LoginPage(),
     );
   }
+}
+
 
 //void main() {
 //  debugInvertOversizedImages = false;
-  
+
 //  runApp(const MeuAplicativo());
 
 //}
--- app_receitas/lib/pages/my_fridge.dart
+++ app_receitas/lib/pages/my_fridge.dart
@@ -1,8 +1,4 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-
-import '../models/receitas.dart';
-import '../repositorie/receitasRepositorie.dart';
 
 // ignore: use_key_in_widget_constructors
 class MyFridgePage extends StatefulWidget {
@@ -27,66 +23,59 @@ class _MyFridgePageState extends State<MyFridgePage> {
           fontSize: tamanho,
           fontWeight: FontWeight.bold,
           color: const Color(0xFF474747),
-            
         ),
       ),
       // ignore: prefer_const_constructors
     );
   }
 
-  Widget _addBar(){
+  Widget _addBar() {
     return Container(
-          margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
-          child: Column(
+      margin: const EdgeInsets.only(top: 25, left: 25, right: 25),
+      child: Column(
+        children: [
+          Row(
             children: [
-              Row(
-                children: [
-                  Flexible(
-                    flex: 1,
-                    child: TextField(
-                      cursorColor: Colors.grey,
-                      decoration: InputDecoration(
-                        fillColor: Colors.white,
-                        filled: true,
-                        border: OutlineInputBorder(
+              Flexible(
+                flex: 1,
+                child: TextField(
+                  cursorColor: Colors.grey,
+                  decoration: InputDecoration(
+                      fillColor: Colors.white,
+                      filled: true,
+                      border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
-                          borderSide: BorderSide.none
-                        ),
-                        hintText: 'Pesquisar um alimento',
-                        hintStyle: const TextStyle(
-                          color: Colors.grey,
-                          fontSize: 18
-                        ),
-                        prefixIcon: Container(
-                          padding: const EdgeInsets.all(15),
-                          width: 18,
-                          child: IconButton(onPressed: (){}, icon: Icon(Icons.add)),
-                        )
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only (left: 10),
-                    padding: const EdgeInsets.all(15),
-                    decoration: BoxDecoration(
+                          borderSide: BorderSide.none),
+                      hintText: 'Pesquisar um alimento',
+                      hintStyle:
+                          const TextStyle(color: Colors.grey, fontSize: 18),
+                      prefixIcon: Container(
+                        padding: const EdgeInsets.all(15),
+                        width: 18,
+                        child: IconButton(
+                            onPressed: () {}, icon: const Icon(Icons.add)),
+                      )),
+                ),
+              ),
+              Container(
+                  margin: const EdgeInsets.only(left: 10),
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
                       color: const Color(0xFFE58F65),
-                      borderRadius: BorderRadius.circular(15)
-                    ),
-                    width: 60,
-                    child: Image.asset(
-                    'assets/images/icons/search_2.png')
-                  ),
-                ],
-              )
+                      borderRadius: BorderRadius.circular(15)),
+                  width: 60,
+                  child: Image.asset('assets/images/icons/search_2.png')),
             ],
-          ),
-        );
+          )
+        ],
+      ),
+    );
   }
 
-    bool isPressed = false;
+  bool isPressed = false;
   _pressed() {
     var newVal = true;
-    if(isPressed) {
+    if (isPressed) {
       newVal = false;
     } else {
       newVal = true;
@@ -94,69 +83,77 @@ class _MyFridgePageState extends State<MyFridgePage> {
 
     // This function is required for changing the state.
     // Whenever this function is called it refresh the page with new value
-    setState((){
+    setState(() {
       isPressed = newVal;
     });
   }
 
-  Widget _bottomBar(){
-    
-    return  BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          color: Colors.white,
-          child: IconTheme(
-            data: const IconThemeData(color: Color.fromARGB(255, 209, 209, 209)),
-            child: Padding(padding: const EdgeInsets.all(1),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(onPressed: () => _pressed(), icon: Icon(Icons.home), color: isPressed ? Color(0xFFE58F65) : Color.fromARGB(255, 209, 209, 209)),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-                  const SizedBox(width: 50,),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.list)),
-                  IconButton(onPressed: (){}, icon: Icon(Icons.person)),
-                ],
-              ),),
-
+  Widget _bottomBar() {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      color: Colors.white,
+      child: IconTheme(
+        data: const IconThemeData(color: Color.fromARGB(255, 209, 209, 209)),
+        child: Padding(
+          padding: const EdgeInsets.all(1),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                  onPressed: () => _pressed(),
+                  icon: const Icon(Icons.home),
+                  color: isPressed
+                      ? const Color(0xFFE58F65)
+                      : const Color.fromARGB(255, 209, 209, 209)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+              const SizedBox(
+                width: 50,
+              ),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.list)),
+              IconButton(onPressed: () {}, icon: const Icon(Icons.person)),
+            ],
           ),
-        );
-  }
-
-  Widget _centerBottomBar(){
-    return
-    FloatingActionButton(onPressed: (){}, 
-          backgroundColor: const Color(0xFFE58F65),
-            child: IconButton(onPressed: (){}, icon: Image.asset('assets/images/icons/fridgey.png'), iconSize: 100,),
-          );
-  }
-
-  Widget _counter(){
-    return Container(
-
+        ),
+      ),
     );
   }
+
+  Widget _centerBottomBar() {
+    return FloatingActionButton(
+      onPressed: () {},
+      backgroundColor: const Color(0xFFE58F65),
+      child: IconButton(
+        onPressed: () {},
+        icon: Image.asset('assets/images/icons/fridgey.png'),
+        iconSize: 100,
+      ),
+    );
+  }
+
+  // Widget _counter(){
+  //   return Container(
+
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     bool keyboardIsOpen = MediaQuery.of(context).viewInsets.bottom != 0;
     return Scaffold(
-        body: ListView(
-          children: <Widget>[
-            _text("Minha Geladeira", 35.0),
-            _text("Adicionar à geladeira", 25.0),
-            _addBar(),
-            _foodCard(),
-            
-          ],
-        ),
-        extendBody: true,
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: Visibility(child:_centerBottomBar(), visible: !keyboardIsOpen),
-        bottomNavigationBar: _bottomBar(),
-
+      body: ListView(
+        children: <Widget>[
+          _text("Minha Geladeira", 35.0),
+          _text("Adicionar à geladeira", 25.0),
+          _addBar(),
+          // _foodCard(),
+        ],
+      ),
+      extendBody: true,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton:
+          Visibility(visible: !keyboardIsOpen, child: _centerBottomBar()),
+      bottomNavigationBar: _bottomBar(),
     );
-
-
   }
 }
