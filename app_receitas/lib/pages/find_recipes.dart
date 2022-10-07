b4f2b98c79a4e9cb4c83752edb30db92a1f4bb5f
Igor Castro <igorpfcastro@gmail.com>
2022-10-07 19:20:47 -0300

    apresentation

--- app_receitas/lib/pages/find_recipes.dart
+++ app_receitas/lib/pages/find_recipes.dart
@@ -62,7 +62,10 @@ class FindRecipesPage extends StatelessWidget {
                                 20),
                           ),
                         ],
-                      ))
+                      )),
+                  const SizedBox(
+                    height: 50,
+                  )
                 ])),
       extendBody: true,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
