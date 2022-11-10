f9e10bafe9368f038e6a001e5d2e847abef14146
Skyerv <amanda@manzoti.com>
2022-11-10 00:16:21 -0300

    feat: setting the environment for firebase

--- app_receitas/lib/main.dart
+++ app_receitas/lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:app_receitas/constants.dart';
 import 'package:app_receitas/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const MaterialApp(
-      home: LoginPage(),
-    ));
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fridgey App',
