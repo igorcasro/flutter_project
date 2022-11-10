49bd401e8b67be60decad6db7075cbb41726edb1
Igor Castro <igorpfcastro@gmail.com>
2022-11-10 19:43:34 -0300

    Revert "Feature/user autentication (#20)" (#21)
    This reverts commit 2548bf69d9e8c92b0badc74a50af974086ccd545.

--- app_receitas/lib/main.dart
+++ app_receitas/lib/main.dart
@@ -1,34 +1,16 @@
 import 'package:app_receitas/constants.dart';
-import 'package:app_receitas/services/auth_service.dart';
-import 'package:app_receitas/widgets/auth_check.dart';
+import 'package:app_receitas/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
-import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (context) => AuthService()),
-    ],
-    child: const MyApp(),
-    )
-  );
-}
+void main() => runApp(const MaterialApp(
+      home: LoginPage(),
+    ));
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Fridgey App',
@@ -36,7 +18,7 @@ class MyApp extends StatelessWidget {
         primaryColor: primaryColor,
         scaffoldBackgroundColor: whiteBackgroundColor,
       ),
-      home: const AuthCheck(),
+      home: const LoginPage(),
     );
   }
 }
